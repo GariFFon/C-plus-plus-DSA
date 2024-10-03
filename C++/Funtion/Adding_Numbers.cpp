014e33dd956e04e495648216cb0d17a9d153f9ff
Gourav Dash <gaurav.dash05@gmail.com>
2024-10-03 22:27:44 +0530

    Update Adding_Numbers.cpp

--- C++/Funtion/Adding_Numbers.cpp
+++ C++/Funtion/Adding_Numbers.cpp
@@ -1,28 +1,27 @@
-//
+
 //  main.cpp
 //  Funtion
-//
-//  Created by Gourav Dash on 08/08/24.
-//
 
-//#include <iostream>
-//#include <string.h>
-//using namespace std;
-//
-//int add(int x,int y)
-//{
-//    return x+y;
-//}
-//
-//int main1()
-//{
-//    int a,b;
-//    cout<<"Enter First Number: ";
-//    cin>>a;
-//    cout<<"Enter Second Number: ";
-//    cin>>b;
-//    
-//    cout<<"The Sum is: "<<add(a, b)<<endl;
-//    
-//    return 0;
-//}
+//  Created by Gourav Dash on 08/08/24.
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int add(int x,int y)
+{
+   return x+y;
+}
+
+int main1()
+{
+   int a,b;
+   cout<<"Enter First Number: ";
+   cin>>a;
+   cout<<"Enter Second Number: ";
+   cin>>b;
+   
+   cout<<"The Sum is: "<<add(a, b)<<endl;
+   
+   return 0;
+}
